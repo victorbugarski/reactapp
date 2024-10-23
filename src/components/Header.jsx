43b8cefabb9e464e093ba5deae06d624f0b6dea2
Victor Bugarski <bugarskivictor@gmail.com>
2024-10-23 12:29:22 +0200

    fixat detaljer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,30 +4,30 @@ const Header = () => {
   return (
     <div>
       <header>
-            <div class="container">
+            <div className="container">
     
                  <a id="logo" href="index.html"><img src="Link.svg" alt=""/></a>
     
-                <nav id="main-menu" class="navbar">
-                    <a class="nav-link" href="#">Features</a>
+                <nav id="main-menu" className="navbar">
+                    <a className="nav-link" href="#">Features</a>
                 </nav>
     
-                <div id="darkmode-toggle-switch" class="btn-toggle-switch">
-                    <span class="label">Dark mode</span>
-                    <label for="Dark mode-switch" class="toggle-switch">
+                <div id="darkmode-toggle-switch" className="btn-toggle-switch">
+                    <span className="label">Dark mode</span>
+                    <label for="Dark mode-switch" className="toggle-switch">
                         <input id="Dark mode-switch" type="checkbox"/>
-                        <span class="slider round"></span>
+                        <span className="slider round"></span>
     
                     </label>
                 </div>
     
-                <a id="auth-signin" href="#" class="btn-primary">
-                    <i class="fa-thin fa-user-large"></i>
+                <a id="auth-signin" href="#" className="btn-primary">
+                    <i className="fa-thin fa-user-large"></i>
                     <span>Sign in / Up</span>
                 </a>
     
-                <button class="btn-mobile">
-                    <i class="fa-regular fa-bars"></i>
+                <button className="btn-mobile">
+                    <i className="fa-regular fa-bars"></i>
                 </button>
             
             
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,30 +4,30 @@ const Hero = () => {
   return (
     <div>
       <section id="hero">
-        <div class="container">
-            <div class="headline">
+        <div className="container">
+            <div className="headline">
                 <h1>Manage All Your Money in One App</h1>
             </div>
                 
             
-            <div class="content">
+            <div className="content">
                 <p>We offer you a new generation of the mobile banking. Save, spend & manage money in your pocket.</p>
-                <div class="buttons">
-                    <a class="btn-download-app" href="#"><img src="Logos/Appstore.svg"/></a>
-                    <a class="btn-download-app" href="#"><img src="Logos/Googleplay.svg"/></a>
+                <div className="buttons">
+                    <a className="btn-download-app" href="#"><img src="Logos/Appstore.svg"/></a>
+                    <a className="btn-download-app" href="#"><img src="Logos/Googleplay.svg"/></a>
                 </div>
 
-                <a href="#" class="discover-more">
-                    <span class="btn-circle">
-                        <i class="fa-solid fa-chevron-down"></i>
+                <a href="#" className="discover-more">
+                    <span className="btn-circle">
+                        <i className="fa-solid fa-chevron-down"></i>
                     </span>
                     <span>Discover more</span>
                 </a>
             </div>
 
-            <div class="images">
-                <img class="img-back" src="Logos/iphone-mybudget.svg" alt="iphone my budget"/>
-                <img class="img-front" src="Logos/iphone.svg" alt="iphone"/>
+            <div className="images">
+                <img className="img-back" src="Logos/iphone-mybudget.svg" alt="iphone my budget"/>
+                <img className="img-front" src="Logos/iphone.svg" alt="iphone"/>
             </div>
         </div>
 
