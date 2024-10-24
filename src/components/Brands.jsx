5e57174b594b7a601f7ae507458ba997787a8310
Victor Bugarski <bugarskivictor@gmail.com>
2024-10-24 11:17:56 +0200

    Lagt in bilder på Brands

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import Logo1 from '../assets/Logos/logo1.svg'
+import Logo2 from '../assets/Logos/logo2.svg'
+import Logo3 from '../assets/Logos/logo3.svg'
+import Logo4 from '../assets/Logos/logo4.svg'
+import Logo5 from '../assets/Logos/logo5.svg'
+import Logo6 from '../assets/Logos/logo6.svg'
 
 const Brands = () => {
   return (
@@ -6,22 +12,22 @@ const Brands = () => {
        <section id="brands">
                 <div className="container">
                    <div id="brand-1" className="brand-box">
-                    <img src="Logos/logo1.svg" alt=""/>
+                    <img src={Logo1} alt=""/>
                    </div>
                    <div id="brand-2" className="brand-box">
-                    <img src="Logos/logo2.svg" alt=""/>
+                    <img src={Logo2} alt=""/>
                    </div>
                    <div id="brand-3" className="brand-box">
-                    <img src="Logos/logo3.svg" alt=""/>
+                    <img src={Logo3} alt=""/>
                    </div>
                    <div id="brand-4" className="brand-box">
-                    <img src="Logos/logo4.svg" alt=""/>
+                    <img src={Logo4} alt=""/>
                    </div>
                    <div id="brand-5" className="brand-box">
-                    <img src="Logos/logo5.svg" alt=""/>
+                    <img src={Logo5} alt=""/>
                    </div>
                    <div id="brand-6" className="brand-box">
-                    <img src="Logos/logo6.svg" alt=""/>
+                    <img src={Logo6} alt=""/>
                    </div>
                 </div>
               
