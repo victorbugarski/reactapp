03667330d3726a0eeb7a77c79fa4269069f5050b
Victor Bugarski <bugarskivictor@gmail.com>
2024-10-24 11:28:03 +0200

    Lagt bilder på HowItWorks

--- src/components/Howitworks.jsx
+++ src/components/Howitworks.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import Vänster from '../assets/Logos/iphone vänster.svg'
+import Mitten from '../assets/Logos/iphone mitten.svg'
+import Höger from '../assets/Logos/iphone höger.svg'
+
 
 const Howitworks = () => {
   return (
@@ -11,15 +15,15 @@ const Howitworks = () => {
 
             <div className="container-2">
                 <div className="iphone">
-                    <img src="Logos/iphone vänster.svg" alt="vänster" className="phone-img"/>
+                    <img src={Vänster} alt="vänster" className="phone-img"/>
                 </div>
 
                 <div className="iphone">
-                    <img src="Logos/iphone mitten.svg" alt="mitten" className="phone-img"/>
+                    <img src={Mitten} alt="mitten" className="phone-img"/>
                 </div>
 
                 <div className="iphone">
-                    <img src="Logos/iphone höger.svg" alt="höger" className="phone-img"/>
+                    <img src={Höger} alt="höger" className="phone-img"/>
                 </div>
             </div>
 
